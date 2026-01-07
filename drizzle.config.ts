2943a94ba4b1a54a7e60c960d919581975fa6c8b
Induktr <nikitavoitenko2020@gmail.com>
2026-01-07 09:40:37 +0200

    chore: Update Drizzle schema path in Drizzle configuration.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_URL) {
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./shared/schemas/schema.ts",
+  schema: "./server/shared/schemas/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
